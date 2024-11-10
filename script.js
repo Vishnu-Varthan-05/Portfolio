39da7c28855706b23a1f5933568df9d4fb1a4744
Vishnuvarthan M <131575256+Vishnu-Varthan-05@users.noreply.github.com>
2024-11-10 16:47:36 +0530

    Changed color and done some major change

--- script.js
+++ script.js
@@ -11,25 +11,55 @@ function waitForInput(inputElement){
 }
 
 async function generateInputAndOuput(){
+    const infoElement = document.createElement("p");
+    infoElement.classList.add("jt-medium", "terminal-output");
+    infoElement.textContent = "Enter ls to start exploring my portfolio..."
+    terminal.appendChild(infoElement);
+    
     let userInput = '';
     while (true) {
         const inputElement = document.createElement("input");
+        const inputLabel =  document.createElement("label");
+
         inputElement.classList.add("jt-medium", "terminal-input");
         inputElement.setAttribute("autofocus", "true");
+        inputElement.name = "inputElement";
+
+        inputLabel.classList.add("jt-medium", "terminal-label");;
+        inputLabel.setAttribute("for", "inputElement");
+        inputLabel.textContent = "->";
+
+        terminal.appendChild(inputLabel)
         terminal.appendChild(inputElement);
+
+        inputElement.focus();
+        
         userInput = await waitForInput(inputElement);
+        inputElement.setAttribute("disabled", "true");
+
         console.log(userInput);
 
-        const outputElement = document.createElement("p");
+        const command = userInput.split(" ")[0];
+        const outputElement = document.createElement("pre");
         outputElement.classList.add("jt-medium", "terminal-output");
-        outputElement.textContent = userInput;
-        terminal.appendChild(outputElement);
+        if (commands[command]) {
+            commands[command](outputElement);
+        }
+        else{
+            outputElement.textContent = "Command not found";
+            terminal.appendChild(outputElement);
+        }       
     }
 }
 
 
 const commands = {
-    quit: "The program will quit",
-    help: "Helped"
+    ls: (outputElement)=>{
+        outputElement.textContent = "about -> to know more about me\nskills -> to know about my skills\nprojects -> to know about the projects\nclear -> to clear the terminal\n";
+        terminal.appendChild(outputElement);
+    },
+    clear: ()=>{
+        terminal.innerHTML = "";
+    },
 }
 
